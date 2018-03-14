6006648664cbe773ac2cdd0ce284212cb1a8b042
Jean-Simon Robert Ouimet <jr@smbsol.com>
2018-03-14 22:35:30 +0900

    Restructured code. Added events controller.

--- contactr.js
+++ contactr.js
@@ -2,22 +2,63 @@
 // Data controller
 // ==============================
 const dataCtrl = (() => {
-  let allContacts = [];
+  let storedContacts = [];
 
   return {
-    allContacts: allContacts,
-    fetchAll: fetchAll
+    fetchContacts: fetchContacts,
+    storedContacts: storedContacts,
+    save: save,
+    update: update,
+    remove: remove
   };
 
-  function fetchAll() {
+  function fetchContacts() {
     const contacts = httpService.get('http://localhost:3000/contacts')
       .then(contacts => {
         contacts.forEach(contact => {
-          allContacts.push(contact);
-        });  
-        uiCtrl.displayAll(contacts);
+          storedContacts.push(contact);
+        });
+        uiCtrl.listAll(storedContacts);
       })
       .catch(err => console.log(err));
+  }
+
+  function save(e) {
+    console.log('Saving new contact!');
+
+    e.preventDefault();
+  }
+
+  function update(e) {
+    const inputValues = document.querySelectorAll(uiCtrl.selectors.inputs);
+    const contacts = dataCtrl.storedContacts;
+    let [keys, values] = [[], []];
+    let updatedContactDetails = {};
+
+    inputValues.forEach(input => {
+      keys.push(input.id);
+      values.push(input.value);
+    });
+
+    keys.forEach((key, i) => {
+      updatedContactDetails[key] = values[i];
+    });
+
+    contacts.forEach(contactDetails => {
+      if(contactDetails.id === parseInt(updatedContactDetails.id)) {
+        contactDetails = updatedContactDetails;
+        debugger;
+        httpService.put(`http://localhost:3000/contacts/${contactDetails.id}`, contactDetails);        
+      }
+    });
+
+    e.preventDefault();
+  }
+
+  function remove(e) {
+    confirm('Are you sure?');
+
+    e.preventDefault();
   }
 })();
 
@@ -32,25 +73,27 @@ const uiCtrl = (() => {
     contactListItemActive: '.active',
     contactDetails: '.contactDetails',
     addBtn: '.addBtn',
-    updateBtn: '.updateBtn',
+    deleteBtn: '.deleteBtn',
     editBtn: '.editBtn',
+    updateBtn: '.updateBtn',
+    saveBtn: '.saveBtn',
     cancelBtn: '.cancelBtn',
+    addCancelBtn: '.addCancelBtn',
     inputs: 'input'
   };
 
   return {
     selectors: selectors,
-    displayAll: displayAll,
+    listAll: listAll,
     displayCurrentDetails: displayCurrentDetails,
-    changeCurrent: changeCurrent,
-    edit: edit
+    changeCurrent: changeCurrent
   };
 
-  function displayAll(contacts) {
+  function listAll(storedContacts) {
     const contactList = document.querySelector(selectors.contactList);
     let currentContact, content = '';
 
-    contacts.forEach(contact => {
+    storedContacts.forEach(contact => {
       content += `
         <li id="${contact.id}" class="contactListItem list-group-item list-group-item-action">
           <span class="contact-name">${contact.firstName} <b>${contact.lastName}</b></span>
@@ -62,17 +105,15 @@ const uiCtrl = (() => {
     document.getElementById('1').classList += ' active';
     currentContact = document.getElementById('1');
     displayCurrentDetails(currentContact.id);
-
-    contactList.addEventListener('click', changeCurrent);
   }
 
   function displayCurrentDetails(id) {
-    const allContacts = dataCtrl.allContacts;
+    const storedContacts = dataCtrl.storedContacts;
     const contactDetails = document.querySelector(selectors.contactDetails);
     const inputs = document.querySelectorAll(selectors.inputs);
     let currentContact, output;
 
-    allContacts.forEach(contact => {
+    storedContacts.forEach(contact => {
       if(contact.id === parseInt(id)) {
         currentContact = contact;
       }
@@ -88,42 +129,44 @@ const uiCtrl = (() => {
             <div class="form-row">
               <input type="hidden" id="id" value="${currentContact.id}">
               <div class="col">
-                <input type="text" readonly class="form-control-plaintext h2 text-right" id="firstName" value="${currentContact.firstName}">
+                <input type="text" readonly class="form-control-plaintext h2 text-right" id="firstName" name="First Name" value="${currentContact.firstName}">
               </div>
               <div class="col">
-                <input type="text" readonly class="form-control-plaintext h2 text-left" id="lastName" value="${currentContact.lastName}">              
+                <input type="text" readonly class="form-control-plaintext h2 text-left" id="lastName" name="Last Name" value="${currentContact.lastName}">              
               </div>
             </div>
             <div class="d-flex justify-content-center">
               <button class="editBtn btn btn-outline-dark btn-sm">Edit</button>
-              <button class="cancelBtn d-none">Cancel</button>              
+              <button class="cancelBtn d-none">Cancel</button>       
               <button class="updateBtn d-none">Update</button>
+              <button class="saveBtn d-none">Save</button>
+              <button class="deleteBtn btn btn-outline-danger btn-sm ml-2">Delete</button>
             </div>
           </div>
         </div>
         <div class="row">
           <label for="phoneNumber" class="col-sm-3 col-form-label-sm font-weight-bold">Phone Number</label>
           <div class="col-sm-9">
-            <input type="text" readonly class="form-control-plaintext form-control-sm" id="phoneNumber" value="${currentContact.phoneNumber}">
+            <input type="text" readonly class="form-control-plaintext form-control-sm" id="phoneNumber" name="Phone Number" value="${currentContact.phoneNumber}">
           </div>
         </div>
         <div class="row">
           <label for="emailAddress" class="col-sm-3 col-form-label-sm font-weight-bold">Email Address</label>
           <div class="col-sm-9">
-            <input type="text" readonly class="form-control-plaintext form-control-sm" id="emailAddress" value="${currentContact.emailAddress}">
+            <input type="text" readonly class="form-control-plaintext form-control-sm" id="emailAddress" name="Email" value="${currentContact.emailAddress}">
           </div>
         </div>
         <hr>
         <div class="row">
           <label for="dob" class="col-sm-3 col-form-label-sm font-weight-bold">Birthday</label>
           <div class="col-sm-9">
-            <input type="text" readonly class="form-control-plaintext form-control-sm" id="dob" value="${currentContact.dob}">
+            <input type="text" readonly class="form-control-plaintext form-control-sm" id="dob" name="Date of Birth" value="${currentContact.dob}">
           </div>
         </div>
         <div class="row">
           <label for="age" class="col-sm-3 col-form-label-sm font-weight-bold">Age</label>
           <div class="col-sm-9">
-            <input type="text" readonly class="form-control-plaintext form-control-sm" id="age" value="${currentContact.age}">
+            <input type="text" readonly class="form-control-plaintext form-control-sm" id="age" name="Remove me!" value="${currentContact.age}">
           </div>
         </div>
         <hr>
@@ -132,23 +175,23 @@ const uiCtrl = (() => {
           <div class="col-sm-9">
             <div class="row">
               <div class="col-sm-6">
-                <input type="text" readonly class="form-control-plaintext form-control-sm" id="street" value="${currentContact.street}">
+                <input type="text" readonly class="form-control-plaintext form-control-sm" id="street" name="Street" value="${currentContact.street}">
               </div>
             </div>
             <div class="form-row">
               <div class="col-4 col-sm-2">
-                <input type="text" readonly class="form-control-plaintext form-control-sm" id="city" value="${currentContact.city}">
+                <input type="text" readonly class="form-control-plaintext form-control-sm" id="city" name="City" value="${currentContact.city}">
               </div>
               <div class="col-4 col-sm-2">
-                <input type="text" readonly class="form-control-plaintext form-control-sm" id="region" value="${currentContact.region}">
+                <input type="text" readonly class="form-control-plaintext form-control-sm" id="region" name="Region" value="${currentContact.region}">
               </div>
               <div class="col-4 col-sm-2">
-                <input type="text" readonly class="form-control-plaintext form-control-sm" id="postalCode" value="${currentContact.postalCode}">
+                <input type="text" readonly class="form-control-plaintext form-control-sm" id="postalCode" name="Postal Code" value="${currentContact.postalCode}">
               </div>
             </div>
             <div class="row">
               <div class="col-sm-12">
-                <input type="text" readonly class="form-control-plaintext form-control-sm" id="country" value="${currentContact.country}">
+                <input type="text" readonly class="form-control-plaintext form-control-sm" id="country" name="Country" value="${currentContact.country}">
               </div>
             </div>
           </div>
@@ -156,8 +199,8 @@ const uiCtrl = (() => {
       </form>`;
 
     contactDetails.innerHTML = output;
-    
-    loadEventListeners();
+
+    eCtrl.loadEventListeners();
   }
 
   function changeCurrent(e) {
@@ -168,153 +211,126 @@ const uiCtrl = (() => {
 
     uiCtrl.displayCurrentDetails(e.target.id);
   }
+})();
+
+
+// ==============================
+// State controller
+// ==============================
+const stateCtrl = (() => {  
+  return {
+    add: add,
+    edit: edit,
+    cancel: cancel,
+    addCancel: addCancel,
+  };
 
   function add() {
-    const contactDetails = document.querySelector(selectors.contactDetails);    
-    let output;
+    const editBtn = document.querySelector(uiCtrl.selectors.editBtn);
+    const deleteBtn = document.querySelector(uiCtrl.selectors.deleteBtn);
+    const cancelBtn = document.querySelector(uiCtrl.selectors.cancelBtn);
+    const saveBtn = document.querySelector(uiCtrl.selectors.saveBtn);
 
-    output = `
-      <form>
-        <div class="jumbotron jumbotron-fluid">
-          <div class="container">
-            <div class="d-flex justify-content-center">
-              <img class="img-thumbnail rounded-circle avatar">
-            </div>
-            <div class="form-row">
-              <input type="hidden" id="id" value="x">
-              <div class="col">
-                <input type="text" class="form-control-plaintext h2 text-right" id="firstName" placeholder="First Name">
-              </div>
-              <div class="col">
-                <input type="text" class="form-control-plaintext h2 text-left" id="lastName" placeholder="Last Name">              
-              </div>
-            </div>
-            <div class="d-flex justify-content-center">
-              <button class="cancelBtn btn btn-outline-danger btn-sm">Cancel</button>              
-              <button class="saveBtn btn btn-outline-success btn-sm ml-2">Add</button>
-            </div>
-          </div>
-        </div>
-        <div class="row">
-          <label for="phoneNumber" class="col-sm-3 col-form-label-sm font-weight-bold">Phone Number</label>
-          <div class="col-sm-9">
-            <input type="text" class="form-control-plaintext form-control-sm" id="phoneNumber" placeholder="Phone Number">
-          </div>
-        </div>
-        <div class="row">
-          <label for="emailAddress" class="col-sm-3 col-form-label-sm font-weight-bold">Email Address</label>
-          <div class="col-sm-9">
-            <input type="text" class="form-control-plaintext form-control-sm" id="emailAddress" placeholder="Email Address">
-          </div>
-        </div>
-        <hr>
-        <div class="row">
-          <label for="dob" class="col-sm-3 col-form-label-sm font-weight-bold">Birthday</label>
-          <div class="col-sm-9">
-            <input type="text" class="form-control-plaintext form-control-sm" id="dob" placeholder="Date of Birth">
-          </div>
-        </div>
-        <hr>
-        <div class="row">
-          <label for="address" class="col-sm-3 col-form-label-sm font-weight-bold">Address</label>
-          <div class="col-sm-9">
-            <div class="row">
-              <div class="col-sm-6">
-                <input type="text" class="form-control-plaintext form-control-sm" id="street" placeholder="Street Number">
-              </div>
-            </div>
-            <div class="form-row">
-              <div class="col-4 col-sm-2">
-                <input type="text" class="form-control-plaintext form-control-sm" id="city" placeholder="City">
-              </div>
-              <div class="col-4 col-sm-2">
-                <input type="text" class="form-control-plaintext form-control-sm" id="region" placeholder="Region">
-              </div>
-              <div class="col-4 col-sm-2">
-                <input type="text" class="form-control-plaintext form-control-sm" id="postalCode" placeholder="Postal Code">
-              </div>
-            </div>
-            <div class="row">
-              <div class="col-sm-12">
-                <input type="text" class="form-control-plaintext form-control-sm" id="country" placeholder="Country">
-              </div>
-            </div>
-          </div>
-        </div>
-      </form>`;
+    const inputs = document.querySelectorAll(uiCtrl.selectors.inputs);
 
-      contactDetails.innerHTML = output;
+    // Hide
+    editBtn.className = 'editBtn d-none';
+    deleteBtn.className = 'deleteBtn d-none';
+
+    // Show
+    cancelBtn.classList = 'cancelBtn btn btn-outline-dark btn-sm d-block';
+    saveBtn.classList = 'saveBtn btn btn-outline-success btn-sm d-block ml-2';
+
+    inputs.forEach(input => {
+      input.value = '';
+      input.removeAttribute('readonly');
+      input.setAttribute('placeholder', input.name);
+    });
   }
 
   function edit(e) {
-    const [inputs, editBtn, updateBtn, cancelBtn] = [
-      document.querySelectorAll(selectors.inputs),
-      document.querySelector(selectors.editBtn),
-      document.querySelector(selectors.updateBtn),
-      document.querySelector(selectors.cancelBtn)
-    ];
+    const editBtn = document.querySelector(uiCtrl.selectors.editBtn);
+    const deleteBtn = document.querySelector(uiCtrl.selectors.deleteBtn);    
+    const updateBtn = document.querySelector(uiCtrl.selectors.updateBtn);
+    const cancelBtn = document.querySelector(uiCtrl.selectors.cancelBtn);
 
-    editBtn.className = 'editBtn d-none';
-    updateBtn.classList = 'updateBtn btn btn-outline-success btn-sm d-block ml-2';
-    cancelBtn.classList = 'cancelBtn btn btn-outline-danger btn-sm d-block';
+    const inputs = document.querySelectorAll(uiCtrl.selectors.inputs);
 
     inputs.forEach(input => {
-      input.removeAttribute('readonly')
-    });
-  }
-
-  function update() {
-    const inputValues = document.querySelectorAll(selectors.inputs);
-    const allContacts = dataCtrl.allContacts;
-    let [keys, values] = [[], []];
-    let updatedContactDetails = {};
-
-    inputValues.forEach(input => {
-      keys.push(input.id);
-      values.push(input.value);
+      input.removeAttribute('readonly');
     });
 
-    keys.forEach((key, i) => {
-      updatedContactDetails[key] = values[i];
-    });
+    // Hide
+    editBtn.className = 'editBtn d-none';
+    deleteBtn.className = 'deleteBtn d-none';
 
-    allContacts.forEach(contactDetails => {
-      if(contactDetails.id === parseInt(updatedContactDetails.id)) {
-        contactDetails = updatedContactDetails;
-        httpService.put(`http://localhost:3000/contacts/${contactDetails.id}`, contactDetails);        
-      }
-    });
+    // Show
+    updateBtn.classList = 'updateBtn btn btn-outline-success btn-sm d-block ml-2';
+    cancelBtn.classList = 'cancelBtn btn btn-outline-dark btn-sm d-block';
+
+    e.preventDefault();
   }
 
   function cancel(e) {
-    const [inputs, editBtn, updateBtn, cancelBtn] = [
-      document.querySelectorAll(selectors.inputs),
-      document.querySelector(selectors.editBtn),
-      document.querySelector(selectors.updateBtn),
-      e.target
-    ];
+    const editBtn = document.querySelector(uiCtrl.selectors.editBtn);
+    const deleteBtn = document.querySelector(uiCtrl.selectors.deleteBtn);    
+    const updateBtn = document.querySelector(uiCtrl.selectors.updateBtn);
+    const cancelBtn = e.target;
+    const saveBtn = document.querySelector(uiCtrl.selectors.saveBtn);    
+
+    const inputs = document.querySelectorAll(uiCtrl.selectors.inputs);    
 
     inputs.forEach(input => {
-      input.setAttribute('readonly', 'readonly')
+      input.removeAttribute('placeholder');
+      // input.value...      
+      input.setAttribute('readonly', true);
     });
+
+    // uiCtrl.displayCurrentDetails();
     
     editBtn.className = 'editBtn btn btn-outline-dark btn-sm d-block';
+    deleteBtn.classList = 'deleteBtn btn btn-outline-danger btn-sm ml-2';
     updateBtn.classList = 'updateBtn d-none';
     cancelBtn.classList = 'cancelBtn d-none';
+    saveBtn.classList = 'saveBtn d-none';
+    
+    e.preventDefault();
   }
 
-  function loadEventListeners() {
-    const [addBtn, editBtn, updateBtn, cancelBtn] = [
-      document.querySelector(selectors.addBtn),
-      document.querySelector(selectors.editBtn),
-      document.querySelector(selectors.updateBtn),
-      document.querySelector(selectors.cancelBtn)
-    ];
+  function addCancel() {
+    console.log('Canceled adding a contact!');
+  }
+})();
 
-    addBtn.addEventListener('click', add);
-    editBtn.addEventListener('click', edit);
-    updateBtn.addEventListener('click', update);
-    cancelBtn.addEventListener('click', cancel);
+
+// ==============================
+// Events controller
+// ==============================
+const eCtrl = (() => {
+  return {
+    loadEventListeners: loadEventListeners
+  };
+
+  function loadEventListeners() {
+    const contactList = document.querySelector(uiCtrl.selectors.contactList);
+    const addBtn = document.querySelector(uiCtrl.selectors.addBtn);
+    const editBtn = document.querySelector(uiCtrl.selectors.editBtn);
+    const deleteBtn = document.querySelector(uiCtrl.selectors.deleteBtn);
+
+    const cancelBtn = document.querySelector(uiCtrl.selectors.cancelBtn);
+    const updateBtn = document.querySelector(uiCtrl.selectors.updateBtn);
+    const saveBtn = document.querySelector(uiCtrl.selectors.saveBtn);
+    const addCancelBtn = document.querySelector(uiCtrl.selectors.addCancelBtn);
+    
+    contactList.addEventListener('click', uiCtrl.changeCurrent);
+    addBtn.addEventListener('click', stateCtrl.add);
+    editBtn.addEventListener('click', stateCtrl.edit);
+    deleteBtn.addEventListener('click', dataCtrl.remove);
+
+    cancelBtn.addEventListener('click', stateCtrl.cancel);
+    updateBtn.addEventListener('click', dataCtrl.update);
+    saveBtn.addEventListener('click', dataCtrl.save);
+    addCancelBtn.addEventListener('click', stateCtrl.addCancel);
   }
 })();
 
@@ -322,6 +338,6 @@ const uiCtrl = (() => {
 // ==============================
 // App controller
 // ==============================
-const appCtrl = ((dataCtrl, uiCtrl) => {
-  dataCtrl.fetchAll();
-})(dataCtrl, uiCtrl);
+const appCtrl = ((dataCtrl, stateCtrl, uiCtrl, eCtrl) => {
+  dataCtrl.fetchContacts();
+})(dataCtrl, stateCtrl, uiCtrl, eCtrl);
