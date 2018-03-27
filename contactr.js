d1c0964ae66a56db0d33ba053b99247776d7bf61
Jean-Simon Robert Ouimet <jouimet@kivuto.com>
2018-03-27 10:19:40 -0400

    Added form buttons.

--- contactr.js
+++ contactr.js
@@ -89,7 +89,10 @@
         storedContacts: storedContacts, // Array
         sortContacts: sortContacts,
         setCurrentContact: setCurrentContact,
-        changeCurrentContact: changeCurrentContact
+        changeCurrentContact: changeCurrentContact,
+        update: update,
+        save: save,
+        delete: del
       };
 
       function sortContacts(a, b) {
@@ -127,6 +130,21 @@
           }
         });
       }
+
+      function update(e) {
+        console.log(e.target);
+        e.preventDefault();
+      }
+
+      function save(e) {
+        console.log(e.target);
+        e.preventDefault();
+      }
+
+      function del(e) {
+        console.log(e.target);
+        e.preventDefault();
+      }
     }
 
     return {
@@ -146,7 +164,9 @@
     function init() {
 
       return {
-        setInitialState: setInitialState
+        setInitialState: setInitialState,
+        edit: edit,
+        cancel: cancel
       };
 
       function setInitialState() {
@@ -166,6 +186,16 @@
             uc.populateContactList(storedContacts);
             uc.displayCurrentContact(storedContacts);
           });
+      }
+
+      function edit(e) {
+        console.log(e.target);
+        e.preventDefault();
+      }
+
+      function cancel(e) {
+        console.log(e.target);
+        e.preventDefault();
       }
     }
 
@@ -189,7 +219,12 @@
         contactListItem: '.contactListItem',
         contactName: '.contactName',
         contactDetails: '.contactDetails',
-        searchInput: '.searchInput'
+        searchInput: '.searchInput',
+        editBtn: '.editBtn',
+        updateBtn: '.updateBtn',
+        cancelBtn: '.cancelBtn',
+        saveBtn: '.saveBtn',
+        deleteBtn: '.deleteBtn'
       };
 
       return {
@@ -242,34 +277,30 @@
       }
 
       function displayCurrentContact(storedContacts) {
+        const ec = EventCtrl.getInstance();
+
         storedContacts.forEach(contact => {
 
+          // Display active contact
           if(contact.active === true) {
             const contactDetails = document.querySelector(selectors.contactDetails);
-            const jumbotronEl = document.createElement('div');
-            const contactDetailsBodyEl = document.createElement('div');
             const formEl = document.createElement('form');
+            const jumbotronEl = document.createElement('div');
+            const formBtn = _buildFormButtons();
+            const contactDetailsBodyEl = document.createElement('div');
 
+            // Discount id, active and empty properties
             for(let key in contact) {
-              if(contact[key] !== '' && key !== 'id' && key !== 'active') {
+              if(key !== 'id' && key !== 'active' && contact[key] !== '') {
                 const fh = FormatHelper.getInstance();
                 const containerEl = document.createElement('div');
                 const rowEl = document.createElement('div');
                 const inputColEl = document.createElement('div');
-                const inputEl = document.createElement('input');
                 const labelEl = document.createElement('label');
+                const inputEl = document.createElement('input');
                 let forVal;
 
-                // Inputs
-                inputEl.id = key;
-                inputEl.classList = 'form-control-plaintext form-control-sm';
-                inputEl.setAttribute('value', contact[key]);
-                inputEl.setAttribute('readonly', true);
-
-                inputColEl.classList = 'col-sm-9';
-                inputColEl.insertAdjacentElement('afterbegin', inputEl);
-
-                // Labels
+                // Build labels
                 labelEl.classList = 'col-sm-3 col-form-label-sm font-weight-bold';
                 labelEl.setAttribute('for', key);
                 labelEl.textContent = key;
@@ -278,10 +309,21 @@
                 rowEl.insertAdjacentElement('afterbegin', labelEl);
                 rowEl.insertAdjacentElement('beforeend', inputColEl);
 
+                // Build inputs
+                inputEl.id = key;
+                inputEl.classList = 'form-control-plaintext form-control-sm';
+                inputEl.setAttribute('value', contact[key]);
+                inputEl.setAttribute('readonly', true);
+
+                inputColEl.classList = 'col-sm-9';
+                inputColEl.insertAdjacentElement('afterbegin', inputEl);
+
+                // Insert into form sections
                 forVal = rowEl.firstChild.attributes[1].value;
                 if(forVal === 'firstName' || forVal === 'lastName') {
                   jumbotronEl.classList = 'jumbotron jumbotron-fluid';
                   jumbotronEl.appendChild(rowEl);
+                  jumbotronEl.appendChild(formBtn);
                 }
                 else {
                   contactDetailsBodyEl.classList = 'contactDetailsBody';
@@ -293,16 +335,62 @@
               }
             }
 
+            // Insert sections into form
             if(contactDetails.hasChildNodes() === false) {
               contactDetails.appendChild(formEl);
             }
-            // If already populated, replace content
             else {
               contactDetails.innerHTML = '';
               contactDetails.appendChild(formEl);
             }
           }
         });
+
+        ec.loadEventListeners();
+
+        function _buildFormButtons() {
+          const rowEl = document.createElement('div');
+          const btnsContent = ['Edit', 'Update', 'Cancel', 'Save', 'Delete'];
+
+          for(let i = 0; i < btnsContent.length; i++) {
+            const btnEl = document.createElement('button');
+
+            btnEl.classList = 'btn btn-sm';
+
+            switch(btnsContent[i]) {
+              case 'Edit':
+              btnEl.classList += ' editBtn btn-outline-dark mr-2';
+              break;
+
+              case 'Update':
+              btnEl.classList += ' updateBtn btn-outline-dark mr-2 d-none';
+              break;
+
+              case 'Cancel':
+              btnEl.classList += ' cancelBtn btn-outline-dark d-none';
+              break;
+
+              case 'Save':
+              btnEl.classList += ' saveBtn btn-outline-success d-none';
+              break;
+
+              case 'Delete':
+              btnEl.classList += ' deleteBtn btn-outline-danger';
+              break;
+
+              default:
+              alert('Not a valid button.');
+            }
+
+            btnEl.textContent = btnsContent[i];
+
+            rowEl.appendChild(btnEl);
+          }
+
+          rowEl.classList = 'd-flex justify-content-center';
+
+          return rowEl;
+        }
       }
 
       function searchContact(e) {
@@ -344,11 +432,27 @@
       function loadEventListeners() {
         const uc = UICtrl.getInstance();
         const dc = DataCtrl.getInstance();
+        const sc = StateCtrl.getInstance();
+
         const contactList = document.querySelector(uc.selectors.contactList);
+        const updateBtn = document.querySelector(uc.selectors.updateBtn);
+        const saveBtn = document.querySelector(uc.selectors.saveBtn);
+        const deleteBtn = document.querySelector(uc.selectors.deleteBtn);
+
         const searchInput = document.querySelector(uc.selectors.searchInput);
 
+        const editBtn = document.querySelector(uc.selectors.editBtn);
+        const cancelBtn = document.querySelector(uc.selectors.cancelBtn);
+
         contactList.addEventListener('click', dc.changeCurrentContact);
+        updateBtn.addEventListener('click', dc.update);
+        saveBtn.addEventListener('click', dc.save);
+        deleteBtn.addEventListener('click', dc.delete);
+
         searchInput.addEventListener('keyup', uc.searchContact);
+
+        editBtn.addEventListener('click', sc.edit);
+        cancelBtn.addEventListener('click', sc.cancel);
       }
     }
 
@@ -434,11 +538,9 @@
 
   const AppCtrl = (function() {
     const sc = StateCtrl.getInstance();
-    const ec = EventCtrl.getInstance();
     const lh = LogHelper.getInstance();
 
     sc.setInitialState();
-    ec.loadEventListeners();
 
     // lh.listAllContacts();
     // lh.listFunctions();
